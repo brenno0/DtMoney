398a3b7ec60ec83c7d08924a1dd7d200d1ea37a8
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-07-21 08:50:30 -0300

    Criação form da modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard';
 import { useState } from 'react';
-import { NewTransactionModal } from './NewTransactionModal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 export function App() {
 
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
